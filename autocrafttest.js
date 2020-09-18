24ecb9ff2222d5f49d68b869cddb33d2b987068e
JanessaMunt <janessamunt@gmail.com>
2020-09-18 03:56:57 -0700

    Update autocrafttest.js

--- autocrafttest.js
+++ autocrafttest.js
@@ -2,39 +2,58 @@ function selectOptions() {
 	$("#optionSelect").toggle();
 };
 
-var craftSteel = false;
+//var craftSteel = false;
 var htmlMenuAddition = '<div id="farRightColumn" class="column">' +
-
+var resources = [
+       		["catnip", "wood", 50, .30, false],
+            ["wood", "beam", 175, .30, false],
+        	["minerals", "slab", 250, .30, false],
+            ["coal", "steel", 100, .30, false],
+        	["iron", "plate", 125, .30, false],
+            ["oil", "kerosene", 7500, .30, false],
+            ["uranium", "thorium", 250, .30, false],
+			["unobtainium", "eludium", 1000, .30, false]
+                ];
 '<a id="scriptOptions" onclick="selectOptions()"> | testscript </a>' +
 
 '<div id="optionSelect" style="display:none; margin-top:-400px; margin-left:-100px; width:200px" class="dialog help">' +
 
 
-'<button id="autoCraft" style="color:red" onclick="autoSwitch(\'autoCraft\')"> Auto Craft Steel </button>' +
+'<button id="autoCraft" style="color:red" onclick="autoSwitch(3, \'autoCraft\')"> Auto Craft Steel </button>' +
 
 '</div>' +
 '</div>'
 
 $("#footerLinks").append(htmlMenuAddition);
 
-function autoSwitch(varName) {
-	if (craftSteel == false) {
-		craftSteel = true;
-		gamePage.msg('Auto' + 'craft Steel' + ' is now on');
+function autoSwitch(resNum, varName) {
+	if (resources[resNum][4] == false) {
+		resources[resNum][4] = true;
+		gamePage.msg('Auto craft' + resources[resNum][0] + ' is now on');
 		document.getElementById(varName).style.color = 'black';
-	} else if (craftSteel == true) {
-		craftSteel = false;
-		gamePage.msg('Auto' + 'craft Steel' + ' is now off');
+	} else if (resources[resNum][4] == true) {
+		resources[resNum][4] = false;
+		gamePage.msg('Auto craft' + resources[resNum][0] + ' is now off');
 		document.getElementById(varName).style.color = 'red';
 	}
 }
 
 
 //autocraft steel
+function autoCraft(){
+	for(int i=0; i < resources.length; i++) { //cycle through all resources
+		var curRes = gamePage.resPool.get(resources[i][0]); //get current amount of resource
+		var resourcePerSec = resourcePerTick * 5; //get resources per second
+	  if(resources[i][4] == true && curRes> (gamePage.resPool.get(resources[i][0]).maxValue - resourcePerSec)){ //check if autocraft is on & if resource is going to hit cap soon
+			var resourcePerCraft = Math.floor(gamePage.resPool.get(resources[i][0]).maxValue/resources[i][2]*resources[i][3]);
+	    gamePage.craft(resources[i][1], resourcePerCraft); //craft resource
+	  }
+	}
+}
+
+//auto run things
 setInterval(function(){
-  if(craftSteel == true){
-    gamePage.craftAll("steel"); //craft all steel
-  }
+	autoCraft(); //autocraft
 }, 1000); //repeat second
 
 //time function
@@ -44,6 +63,24 @@ if (!game.realUpdateModel) game.realUpdateModel = game.updateModel;
 game.updateModel = () => {
     for (var i = 0; i < speed; i++) {
         if (i !== 0) {
+            game.calendar.tick();
+        }
+        game.realUpdateModel();
+    }
+}
+setSpeed = spd => {
+    if (spd >= 1) {
+        speed = spd;
+        updateSpeedText();
+    }
+}
+speedUp = () => setSpeed(speed * 2);
+slowDown = () => setSpeed(speed / 2);
+$("#timeSetting").remove();
+$('#gamePageContainer').append($('<div id="timeSetting" style="position: absolute; top: 50px; right: 10px;" onclick="event.preventDefault(); speedUp();" oncontextmenu="event.preventDefault(); slowDown();">'));
+updateSpeedText = () => $("#timeSetting").html("Speed: " + speed + "x" + (speed > 30 ? " <br />(right click<br />to lower)" : ""));
+updateSpeedText(); */
+
             game.calendar.tick();
         }
         game.realUpdateModel();
