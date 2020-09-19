bd57452055d188516156282aceaff7260a16089b
JanessaMunt <janessamunt@gmail.com>
2020-09-19 02:57:52 -0700

    Add files via upload

--- kittenscriptmenutest.js
+++ kittenscriptmenutest.js
@@ -6,45 +6,91 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-var autoBcoinEnable = false;
-var autoHuntEnable = false;
-var autoTradeEnable = false;
-var autoPraiseEnable =false;
-var autoTradeLeviathansEnable = false;
-var autoTradeNagasEnable = [false, .85];
+
+var autoList = ["bcoin", "hunt", "trade", "praise", "tradeLeviathans", "tradeNaga"]
+
+class autoEnabled {
+	constructor(name){
+		this.name = name;
+		this.enabled = false;
+		this.other = null;
+	}
+}
+
+//create objects to check for auto on/off out of list
+for(var i=0; i<autoList.length; i++){
+  eval(autoList[i] + " = new autoEnabled(\"" + autoList[i] + "\")");
+}
+
+tradeNaga.other = .90;
 
 var resources = [
-       		["catnip", "wood", 50, .30, false, .95],
-            ["wood", "beam", 175, .30, false, .95],
-        	["minerals", "slab", 250, .30, false, .95],
-            ["coal", "steel", 100, .30, false, .95],
-        	["iron", "plate", 125, .30, false, .95],
-            ["oil", "kerosene", 7500, .30, false, .95],
-            ["uranium", "thorium", 250, .30, false, .95],
-			["unobtainium", "eludium", 1000, .30, false, .95]
-                ];
+  ["catnip", "wood", 50, .30, false, .95],
+  ["wood", "beam", 175, .30, false, .95],
+	["minerals", "slab", 250, .30, false, .95],
+  ["coal", "steel", 100, .30, false, .95],
+  ["iron", "plate", 125, .30, false, .95],
+  ["oil", "kerosene", 7500, .30, false, .95],
+  ["uranium", "thorium", 250, .30, false, .95],
+	["unobtainium", "eludium", 1000, .30, false, .95]
+];
 
 var htmlMenuAddition = '<div id="farRightColumn" class="column">' +
 '<a id="scriptOptions" onclick="selectOptions()"> | testscript </a>' +
 
-'<div id="optionSelect" style="display:none; margin-top:-400px; margin-left:-100px; width:200px" class="dialog help">' +
+'<div id="optionSelect" style="display:none; margin-top:-500px; margin-left:-100px; width:250px" class="dialog help">' +
 '</div>' +
 '</div>'
 $("#footerLinks").append(htmlMenuAddition);
 
 var div = document.getElementById('optionSelect');
-div.innerHTML += '<button id="autoBcoinButton" style="color:red" onclick="autoSwitch(autoBcoinEnable, \'autoBcoinButton\')" style= "float:left"> Auto Trade BCoin </button>' ;//button for auto Bcoin traiding
-//'	div.innerHTML += <button id="autoHunt" style="color:red" onclick="autoSwitch(3, \'autoHunt\')"> Auto Hunt </button>' ;
-div.innerHTML += '<button id="autoTradeNagasButton" style="color:red" onclick="autoSwitch(autoTradeNagasEnable[0], \'autoTradeNagasButton\')"> Auto Trade Nagas </button>' ;
-div.innerHTML += '<button id="autoTradeLeviathansButton" style="color:red" onclick="autoSwitch(autoTradeLeviathansEnable, \'autoTradeLeviathansButton\')"> Auto Trade Leviathans </button>' ;
-//	div.innerHTML += '<button id="autoTrade" style="color:red" onclick="autoSwitch(3, \'autoTrade\')"> Auto Trade </button>' ;
-div.innerHTML += '<button id="autoPraiseButton" style="color:red" onclick="autoSwitch(autoPraiseEnable, \'autoPraiseButton\')"> Auto Praise</button>' ;
+for(var i=0; i<autoList.length; i++){
+	div.innerHTML += '<button id="newbutton" style="color:red" style= "float:left" class="btnContent" > Placeholder </button><br>' ;
+	document.getElementById('newbutton').setAttribute("onclick","autoSwitch(\'" + autoList[i] +"\',\'" + "auto"  + autoList[i]  + "\'" + ")" );
+	document.getElementById("newbutton").innerHTML = "Auto " + capitalizeFirstLetter(autoList[i]);
+	document.getElementById("newbutton").id = "auto" + autoList[i];
+}
 
+
+//toggle buttons
+function autoSwitch(objectName, buttonName) {
+	if (window[objectName].enabled == false) { //check if autocraft off on for this resource
+		window[objectName].enabled = true; //turn on autocraft
+		gamePage.msg(document.getElementById(""+buttonName+"").innerHTML + ' is now on'); //pring autocraft is on message
+		document.getElementById(""+buttonName+"").style.color = 'black'; //change button to black
+	}
+	else if (window[objectName].enabled == true) { //check if autocraftis on for this resource
+		window[objectName].enabled = false; //turn off autocraft
+		gamePage.msg(document.getElementById(""+buttonName+"").innerHTML + ' is now off'); //print autocraft is off message
+		document.getElementById(""+buttonName+"").style.color = 'red'; //change button to red
+	}
+}
+
+/*
+var div = document.getElementById('optionSelect');
+for(var i=0; i<autoEnabled.length; i++){
+	div.innerHTML += '<button id="newbutton" style="color:red" style= "float:left"> Placeholder </button><br>' ;
+	document.getElementById('newbutton').setAttribute("onclick","autoSwitch(\'" + i +"\',\'" + "auto"  + autoEnabled[i][0]  + "\'" + ")" );
+	document.getElementById("newbutton").innerHTML = "Auto " + capitalizeFirstLetter(autoEnabled[i][0]);
+	document.getElementById("newbutton").id = "auto" + autoEnabled[i][0];
+}
+*/
+/*
+div.innerHTML += '<button id="autoBcoinButton" style="color:red" onclick="autoSwitch(0, \'autoBcoinButton\')" style= "float:left"> Auto Trade BCoin </button>' ;//button for auto Bcoin traiding
+div.innerHTML += '<button id="autoHunt" style="color:red" onclick="autoSwitch(1, \'autoHunt\')"> Auto Hunt </button>' ;
+
+div.innerHTML += '<button id="autoTradeNagasButton" style="color:red" onclick="autoSwitch(5, \'autoTradeNagasButton\')"> Auto Trade Nagas </button>' ;
+div.innerHTML += '<span id="nagaFillAmount" title="Between 0 and 100"><input id="nagaInput" style="width:25px" onchange="autoEnabled[5][2] = this.value/100" value="30"></span>';
+
+div.innerHTML += '<button id="autoTradeLeviathansButton" style="color:red" onclick="autoSwitch(4, \'autoTradeLeviathansButton\')"> Auto Trade Leviathans </button>' ;
+//	div.innerHTML += '<button id="autoTrade" style="color:red" onclick="autoSwitch(3, \'autoTrade\')"> Auto Trade </button>' ;
+div.innerHTML += '<button id="autoPraiseButton" style="color:red" onclick="autoSwitch(3, \'autoPraiseButton\')"> Auto Praise</button>' ;
+*/
 
 for(var i=0; i<resources.length; i++){
 	var resourceName = capitalizeFirstLetter(resources[i][1]);
 	//toggle buttons for auto crafting each basic resource
-	div.innerHTML += '<button id="newbutton" style="color:red" onclick="placeholder" style= "float:left"> placeholder </button>';  //autopopulate buttons
+	div.innerHTML += '<button id="newbutton" style="color:red" onclick="placeholder"  style="width:150px"> placeholder </button>';  //autopopulate buttons
 	document.getElementById('newbutton').setAttribute("onclick","autoCraftingSwitch(\'" + i +"\',\'" + "auto"  + resourceName  + "\'" + ")" );
 	document.getElementById("newbutton").innerHTML = "Autocraft " + resourceName;
 	document.getElementById("newbutton").id = "auto" + resourceName;
@@ -56,13 +102,13 @@ for(var i=0; i<resources.length; i++){
 	//input for what % of each resource to craft
 	div.innerHTML += '<span id="newspan" title="Between 0 and 100"><input id="newinput" style="width:25px" onchange="placeholder" value="30"></span>';  //autopopulate inputs
 	document.getElementById("newspan").id = "auto" + resourceName + "Span";
-	document.getElementById("newinput").setAttribute("onchange","resources[i][3] = this.value/100");
+	document.getElementById("newinput").setAttribute("onchange","resources["+i+"][3] = this.value/100");
 	document.getElementById("newinput").id = "auto" + resourceName + "input";
 
 	//input for threshold of resource to craft at
-	div.innerHTML += '<span id="newspan" title="Between 0 and 100"><input id="newinput" style="width:25px" onchange="placeholder" value="30"></span><br>';  //autopopulate inputs
+	div.innerHTML += '<span id="newspan" title="Between 0 and 100"><input id="newinput" style="width:25px" onchange="placeholder" value="95"></span><br>';  //autopopulate inputs
 	document.getElementById("newspan").id = "auto" + resourceName + "SpanThreshold";
-	document.getElementById("newinput").setAttribute("onchange","resources[i][5] = this.value/100");
+	document.getElementById("newinput").setAttribute("onchange","resources[" + i + "][5] = this.value/100");
 	document.getElementById("newinput").id = "auto" + resourceName + "inputThreshold";
 }
 
@@ -76,20 +122,21 @@ for(var i=0; i<resources.length; i++){
 '<span id="autoSteelSpan" title="Between 0 and 100"><input id="autoSteelText" type="text" style="width:25px" onchange="resources[3][3] = this.value/100" value="30"></span></br></br>' + //input for % of steel to craft
 */
 
-
-function autoSwitch(enabled, buttonName) {
-	if (enabled == false) { //check if autocraft off on for this resource
-		enabled = true; //turn on autocraft
-		console.log(enabled);
+/*
+function autoSwitch(on, buttonName) {
+	if (autoEnabled[on][1] == false) { //check if autocraft off on for this resource
+		autoEnabled[on][1] = true; //turn on autocraft
 		gamePage.msg(document.getElementById(""+buttonName+"").innerHTML + ' is now on'); //pring autocraft is on message
 		document.getElementById(""+buttonName+"").style.color = 'black'; //change button to black
 	}
-	else if (enabled == true) { //check if autocraftis on for this resource
-		enabled = false; //turn off autocraft
+	else if (autoEnabled[on][1] == true) { //check if autocraftis on for this resource
+		autoEnabled[on][1] = false; //turn off autocraft
 		gamePage.msg(document.getElementById(""+buttonName+"").innerHTML + ' is now off'); //print autocraft is off message
 		document.getElementById(""+buttonName+"").style.color = 'red'; //change button to red
 	}
 }
+
+*/
 
 //toggle crafting on/off
 function autoCraftingSwitch(resNum, varName) {
@@ -110,7 +157,7 @@ function autoCraft(){
 		var curRes = gamePage.resPool.get(resources[i][0]).value; //get current amount of resource
 		var resourcePerSec = gamePage.getResourcePerTick(resources[i][0], 0) * 5; //get resources per second
 		var triggerAutoCraft = resources[i][5]; //calculate how full the resource needs to be to trigger autocraft
-	  if(resources[i][4] == true && curRes> (gamePage.resPool.get(resources[i][0]).maxValue)*triggerAutoCraft){ //check if autocraft is on & if resource is at threshold to auto craft
+	  if(resources[i][4] == true && curRes> (gamePage.resPool.get(resources[i][0]).maxValue)*resources[i][5]){ //check if autocraft is on & if resource is at threshold to auto craft
 			var resourcePerCraft = Math.floor((gamePage.resPool.get(resources[i][0]).maxValue)/(resources[i][2])*(resources[i][3])); //calculate how many crafts to do
 			if(gamePage.craft(resources[i][1], resourcePerCraft) == undefined){ //handles when there is not enough iron to make steel
 				gamePage.craftAll(resources[i][1])
@@ -124,11 +171,13 @@ function autoCraft(){
 
 //autobcoin
 function autoBcoin(){
-	if(this.game.calendar.cryptoPrice> 1095 ){ //check if bcoin price is over 1095
-	self.game.diplomacy.sellBcoin(); //sellbcoin
-	}
-	if(this.game.calendar.cryptoPrice < 900){ //check if bcoin price is under 900
-	self.game.diplomacy.buyBcoin(); //buybcoin
+	if(bcoin.enabled == true){
+		if(this.game.calendar.cryptoPrice> 1095 && gamePage.resPool.get("blackcoin").value > 0){ //check if bcoin price is over 1095
+		self.game.diplomacy.sellBcoin(); //sellbcoin
+		}
+		if(this.game.calendar.cryptoPrice < 900 && gamePage.resPool.get("relic").value > this.game.calendar.cryptoPrice){ //check if bcoin price is under 900
+		self.game.diplomacy.buyBcoin(); //buybcoin
+		}
 	}
 }
 
@@ -139,7 +188,7 @@ function autoTrade(){
 
 //auto trade leviathans
 function autoTradeLeviathans(){
-  if(autoTradeLeviathansEnable == true && gamePage.resPool.get("unobtainium").value > 5000){//check if Leviathan trade is enabled and if enough unobtanium for trade
+  if( tradeLeviathans.enabled == true && gamePage.resPool.get("unobtainium").value > 5000){//check if Leviathan trade is enabled and if enough unobtanium for trade
   gamePage.diplomacy.tradeAll(game.diplomacy.get("leviathans"), Math.floor(gamePage.resPool.get("unobtainium").value/5000)); //trade with levi
 	}
 	else{
@@ -150,7 +199,7 @@ function autoTradeLeviathans(){
 
 //auto trade Nagas
 function autoTradeNagas(){
-	if(autoTradeNagasEnable[0] == true && gamePage.resPool.get("minerals").value < autoTradeNagasEnable[1]*(gamePage.resPool.get("minerals").maxValue)){ //check if enabled and if under mineral threshold
+	if(tradeNaga.enabled == true && gamePage.resPool.get("minerals").value < tradeNaga.other*(gamePage.resPool.get("minerals").maxValue)){ //check if enabled and if under mineral threshold
 	  gamePage.diplomacy.tradeAll(game.diplomacy.get("nagas"), 2878); //trade with naga
 	 }
 	else{
@@ -161,7 +210,7 @@ function autoTradeNagas(){
 
 //autohunt
 function autoHunt(){
-	if (autoHuntEnable== true && gamePage.resPool.get("manpower").value > (gamePage.resPool.get("manpower").maxValue - gamePage.resPool.get("manpower").perTickCashed *5)) {
+	if (hunt.enabled == true && gamePage.resPool.get("manpower").value > (gamePage.resPool.get("manpower").maxValue - gamePage.resPool.get("manpower").perTickCashed *5)) {
 		gamePage.village.huntAll();
 	}
 	else{
@@ -172,7 +221,7 @@ function autoHunt(){
 
 //autopraise
 function autoPraise(){
-	if(autoPraiseEnable == true){
+	if(praise.enabled == true){
 		gamePage.religion.praise();
 	}
 	else{
@@ -186,6 +235,7 @@ setInterval(function(){
 	autoBcoin(); //auto Bcoin
 	autoTrade();
 	autoTradeLeviathans(); //auto trade Leviathans
+	autoTradeNagas();
 	autoHunt(); //auto Hunt
 	autoPraise(); // auto Praise
 }, 1000); //repeat every second
